8c0e5d2745a9365d53b9ed232395cd524a67f677
GioCont <giovanni.contursi03@gmail.com>
2024-11-22 15:51:57 +0100

    Cambio del nome del package

--- src/main/java/progetto_ing_software/Main.java
+++ src/main/java/progetto_ing_software/Main.java
@@ -16,7 +16,7 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
                 System.out.println("Print di prova");
-                        System.out.println("Print di prova2222");
+                        System.out.println("terza prova per la modifica");
 
     }
     
