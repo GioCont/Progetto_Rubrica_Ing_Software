5a8734358d4f6ab692bc2f243c28c5e27eb3cd4a
GioCont <giovanni.contursi03@gmail.com>
2024-12-06 18:02:50 +0100

    Modifica documentazione doxygen

---
+++ src/main/java/progetto_ing_software/Rubricagui.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package progetto_ing_software;
+
+import javax.swing.JFrame;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author Giovanni
+ */
+public class Rubricagui {
+
+ 
+    
+    
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("work in progress");
+                     
+
+    }
+    
+}
