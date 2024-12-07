c1e87b8fb405e37d6edfb8baddc4a8c8f964734d
cerronefederico <cerronefederico.1@gmail.com>
2024-12-07 16:56:29 +0100

    main_con_fxml

--- src/main/java/progetto_ing_software/InterfacciaController.java
+++ src/main/java/progetto_ing_software/InterfacciaController.java
@@ -1,72 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package progetto_ing_software;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
-import javafx.scene.control.cell.PropertyValueFactory;
+
 /**
+ * FXML Controller class
  *
  * @author cerro
  */
-public class InterfacciaController implements Initializable{
-    
-    @FXML
-    private TextField cognomeField;
-    @FXML
-    private TextField nomeField;
-    @FXML
-    private TextField telefono1Field;
-    @FXML
-    private TextField telefono2Field;
-    @FXML
-    private TextField telefono3Field;
-    @FXML
-    private TextField email1Field;
-    @FXML
-    private TextField email2Field;
-    @FXML
-    private TextField email3Field;
-    @FXML
-    private Button salvaButton;
-    private TableView<Contatto> contattiTable;
-    private TableColumn<Contatto, String> cognomeClm;
-    private TableColumn<Contatto, String> nomeClm;
-    
-    private ObservableList<Contatto> contatti;
-    private Rubrica rubrica;
-    
+public class InterfacciaController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
-        
-        contatti = FXCollections.observableArrayList();
-        
-        cognomeClm.setCellValueFactory(new PropertyValueFactory("cognome"));
-        nomeClm.setCellValueFactory(new PropertyValueFactory("nome"));
-        
-        contattiTable.setItems(contatti);
-        rubrica=new Rubrica();
-           
-    } 
-    
-    private void addContatto (ActionEvent event) {
-        
-        contatti.add(new Contatto(nomeField.getText(),cognomeField.getText(),telefono1Field.getText(),email1Field.getText()));
-        
-        
-        
-    }
-    
+        // TODO
+    }    
     
 }
