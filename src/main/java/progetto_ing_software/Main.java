3a82da49a9382eba5c7ecbdd936becd265aab3fd
cerronefederico <cerronefederico.1@gmail.com>
2024-12-07 12:45:07 +0100

    main_con_fxml

--- src/main/java/progetto_ing_software/Main.java
+++ src/main/java/progetto_ing_software/Main.java
@@ -4,21 +4,32 @@
  */
 package progetto_ing_software;
 
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 /**
  *
  * @author Giovanni
  */
-public class Main {
+public class Main extends Application{
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Interfaccia.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        
-        System.out.println("work in progress");
-                     
-
+        launch(args);
     }
     
 }
