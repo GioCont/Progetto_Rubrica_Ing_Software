2eae1890b11c0dfac68262cb3cbecad173fcce99
GioCont <giovanni.contursi03@gmail.com>
2024-12-09 15:21:20 +0100

    Add files via upload

---
+++ Progetto_Rubrica/src/main/java/com/progetto/progetto_rubrica/Progetto_Rubrica.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.progetto.progetto_rubrica;
+
+/**
+ *
+ * @author Giovanni
+ */
+public class Progetto_Rubrica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
