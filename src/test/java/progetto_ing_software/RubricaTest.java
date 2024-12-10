74afd84fbdb25931fb043464bc77c97af59a28f6
Giovanni <giovanni.contursi03@gmail.com>
2024-12-10 16:06:06 +0100

    Aggiunta Test per la rubrica nei TestPackages

---
+++ src/test/java/progetto_ing_software/RubricaTest.java
@@ -0,0 +1,209 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package progetto_ing_software;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Giovanni
+ */
+public class RubricaTest {
+    
+    public RubricaTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getContatti method, of class Rubrica.
+     */
+    @Test
+    public void testGetContatti() {
+        System.out.println("getContatti Test");
+        List<Contatto> prova = new ArrayList<Contatto>();
+        Rubrica rubricaTest = new Rubrica();
+        String[] numTel1 = {"tel12","tel22","tel32"};
+        String[] email1 = {"email12","email22","email32"};
+        Contatto a= new Contatto("Federico","Cerrone",numTel1,email1);
+        String[] numTel2 = {"tel13","tel23","tel33"};
+        String[] email2 = {"email13","email23","email33"};
+        Contatto b= new Contatto("Federico","Cerrone",numTel2,email2);
+        String[] numTel3 = {"tel14","tel24","tel34"};
+        String[] email3 = {"email14","email24","email34"};
+        Contatto c= new Contatto("Federico","Cerrone",numTel3,email3);
+        rubricaTest.addContatto(a);
+        rubricaTest.addContatto(b);
+        rubricaTest.addContatto(c);
+        prova = rubricaTest.getContatti();
+        assertEquals(prova,rubricaTest.getContatti());
+        assertNotNull(prova);
+    }
+
+    /**
+     * Test of addContatto method, of class Rubrica.
+     */
+    @Test
+    public void testAddContatto() {
+        System.out.println("addContatto Test");
+        List<Contatto> prova = new ArrayList<Contatto>();
+        Rubrica rubricaTest = new Rubrica();
+        String[] numTel1 = {"tel12","tel22","tel32"};
+        String[] email1 = {"email12","email22","email32"};
+        Contatto a= new Contatto("Federico","Cerrone",numTel1,email1);
+        rubricaTest.addContatto(a);
+        assertNotNull(rubricaTest);
+        prova.add(a);
+        assertEquals(prova,rubricaTest.getContatti());
+    }
+
+    /**
+     * Test of removeContatto method, of class Rubrica.
+     */
+    @Test
+    public void testRemoveContatto() {
+        System.out.println("removeContatto Test");
+        List<Contatto> prova = new ArrayList<Contatto>();
+        Rubrica rubricaTest = new Rubrica();
+        String[] numTel1 = {"121212","222222","33333"};
+        String[] email1 = {"email12","email22","email222"};
+        Contatto a= new Contatto("Nicolas","De Stefano",numTel1,email1);
+        String[] numTel2 = {"2233223","4444","999999"};
+        String[] email2 = {"emai7777","emai3333","email6566"};
+        Contatto b= new Contatto("Nicolas","De Stefano",numTel2,email2);
+        String[] numTel3 = {"66666","767677","3232323"};
+        String[] email3 = {"email65566","email43434","emai333333"};
+        Contatto c= new Contatto("Pierluigi","Cinque",numTel3,email3);
+        rubricaTest.addContatto(a);
+        rubricaTest.addContatto(b);
+        rubricaTest.addContatto(c);
+        prova.add(a);
+        prova.add(b);
+        rubricaTest.removeContatto(c);
+        assertEquals(prova,rubricaTest.getContatti());
+    }
+
+    /**
+     * Test of modificaContatto method, of class Rubrica.
+     */
+    @Test
+    public void testModificaContatto() {
+        System.out.println("modificaContatto Test");
+        List<Contatto> prova = new ArrayList<Contatto>();
+        Rubrica rubricaTest = new Rubrica();
+        String[] numTel1 = {"1111111","222222","333333"};
+        String[] email1 = {"prova@1","prova@2","prova@3"};
+        Contatto a= new Contatto("Nicolas","De Stefano",numTel1,email1);
+        String[] numTel2 = {"444444","55555","66666"};
+        String[] email2 = {"prova@4","prova@5","prova@6"};
+        Contatto b= new Contatto("Giovanni","Contursi",numTel2,email2);
+        String[] numTel3 = {"777777","888888","99999"};
+        String[] email3 = {"prova@7","prova@8","prova@9"};
+        Contatto c= new Contatto("Pierluigi","Cinque",numTel3,email3);
+        rubricaTest.addContatto(a);
+        rubricaTest.addContatto(b);
+        rubricaTest.addContatto(c);
+        String[] numTel4 = {"1212121","2323232","3434343"};
+        String[] email4 = {"prova@10","prova@11","prova@12"};
+        Contatto d= new Contatto("Federico","Cerrone",numTel1,email1);
+        prova.add(d);
+        prova.add(c);
+        prova.add(b);
+        rubricaTest.modificaContatto(0, d);
+        assertEquals(prova, rubricaTest.getContatti());
+        
+    }
+
+    /**
+     * Test of salvaRubrica method, of class Rubrica.
+     */
+    @Test
+    public void testSalvaRubrica() {
+        System.out.println("salvaRubrica");
+        List<Contatto> prova = new ArrayList<Contatto>();
+        Rubrica rubricaTest = new Rubrica();
+        String[] numTel1 = {"1111111","222222","333333"};
+        String[] email1 = {"prova@1","prova@2","prova@3"};
+        Contatto a= new Contatto("Giovanni","Contursi",numTel1,email1);
+        rubricaTest.addContatto(a);
+        prova.add(a);
+        rubricaTest.salvaRubrica();
+        assertEquals(prova,rubricaTest.getContatti());
+    }
+
+    /**
+     * Test of caricaRubrica method, of class Rubrica.
+     */
+    @Test
+    public void testCaricaRubrica() {
+        System.out.println("caricaRubrica Test");
+        Rubrica rubricaTest = new Rubrica();
+        Rubrica secondaRubrica = new Rubrica();
+        String[] numTel1 = {"1111111","222222","333333"};
+        String[] email1 = {"prova@1","prova@2","prova@3"};
+        Contatto a= new Contatto("Nicolas","De Stefano",numTel1,email1);
+        String[] numTel2 = {"444444","55555","66666"};
+        String[] email2 = {"prova@4","prova@5","prova@6"};
+        Contatto b= new Contatto("Giovanni","Contursi",numTel2,email2);
+        String[] numTel3 = {"777777","888888","99999"};
+        String[] email3 = {"prova@7","prova@8","prova@9"};
+        Contatto c= new Contatto("Pierluigi","Cinque",numTel3,email3);
+        rubricaTest.addContatto(a);
+        rubricaTest.addContatto(b);
+        rubricaTest.addContatto(c);
+        rubricaTest.salvaRubrica();
+        secondaRubrica.caricaRubrica();
+        assertNotNull(secondaRubrica);
+
+    }
+
+    /**
+     * Test of ricercaContatti method, of class Rubrica.
+     */
+    @Test
+    public void testRicercaContatti() {
+        System.out.println("ricercaContatti Test");
+        Rubrica rubricaTest = new Rubrica();
+        List<Contatto> prova = new ArrayList<Contatto>();
+        List<Contatto> rif = new ArrayList<Contatto>();
+        String[] numTel1 = {"1111111","222222","333333"};
+        String[] email1 = {"prova@1","prova@2","prova@3"};
+        Contatto a= new Contatto("Nicolas","De Stefano",numTel1,email1);
+        String[] numTel2 = {"444444","55555","66666"};
+        String[] email2 = {"prova@4","prova@5","prova@6"};
+        Contatto b= new Contatto("Giovanni","Contursi",numTel2,email2);
+        String[] numTel3 = {"777777","888888","99999"};
+        String[] email3 = {"prova@7","prova@8","prova@9"};
+        Contatto c= new Contatto("Pierluigi","Cinque",numTel3,email3);
+        rubricaTest.addContatto(a);
+        rubricaTest.addContatto(b);
+        rubricaTest.addContatto(c);
+        prova.add(b);
+        rif = rubricaTest.ricercaContatti("Giovanni");
+        assertEquals(rif.get(0),prova.get(0));
+    }
+    
+}
